7cd1aae6fa15cdd56857d8da5862a87d91f87a90
R.Guo <ray.guo.inchina@gmail.com>
2018-08-10 11:33:47 +0800

    submit the functions first time

---
+++ functions/funtion.m
@@ -0,0 +1,29 @@
+% we can type directly the myRand to realize the funtion
+% myRand
+%where a is local variable which is defined only in the funtion myRand, and it has no effect on
+% the cammand window workspace. print a, it will show 'Undefined function or variable 'a'.
+a
+myRand
+
+% function output
+% where a is local variable which is defined only in the funtion myRand, and it has no effect on
+% the cammand window workspace. print a, it will show 'Undefined function or variable 'a'.
+% to identify where the user-defined function can output into a variable
+% b = myRand
+% b
+% a
+
+% function input
+% apply parameters to the functions
+% myRand(5,10)
+% or: x = 0.5, y=1, z = myRand(x,y)
+
+% multiple Output
+% myRand(1,2)
+% x = myRand(-1,1)
+% [x,y] = myRand(0,1)
+
+% subfunction
+% myRand(-5,5)
+% s
+% a
---
+++ functions/myRand.m
@@ -0,0 +1,43 @@
+% user-defined function
+
+% If your function is quite complex and long, you may want to break it up
+% to smaller pieces; if those pieces are not expected to be useful by
+% themselves other than in the context of this complicated function, we
+% can create them as subfunctions
+
+% functions
+
+%function myRand 
+%a = 9*rand(3,4)+3;
+%end
+  
+% function  output
+% We can get anything out of a funciton and into a variable in the command window
+% function a = myRand 
+% a = 9*rand(3,4)+3;
+% end
+
+% function  input
+% We can supply parameters to the function
+% function a = myRand(low,, high) 
+% a = (high-low) * rand(3,4) + low;
+% end
+
+% multiple Outputs
+% After input multiple parameters to a function, it's possible to obtain
+% multiple results for matlat, unlike C, C++, Java et al.
+% function [a,s] = myRand(low, high)
+% a = (high-low) * rand(3,4) + low;
+% s = sum(a(:));
+
+% formal definition
+% remember the name rules for m-files;
+
+% subfunctions
+
+% Call our own functions from within each other
+% function [a, s] = myRand(low, high)
+% a = (high-low)*rand(3,4) + low;
+% s = sumALLElement(a);
+% function summa = sumALLElement(M)
+% summa = sum(M(:));
